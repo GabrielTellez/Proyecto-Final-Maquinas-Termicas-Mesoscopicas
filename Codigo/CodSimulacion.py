5d5a0ebb1a5c0bf8ca2637b42dcd39385fa5c3d2
Gabriel Tellez <gtellez@uniandes.edu.co>
2022-12-15 23:33:58 -0500

    Comentarios preliminares al código (GT)

--- Codigo/CodSimulacion.py
+++ Codigo/CodSimulacion.py
@@ -12,7 +12,7 @@ def proceso(l, Delta_s, kappa, theta, x0):
     x = np.zeros(l)
     x[0] = x0
 
-    if(x0 == 1):
+    if(x0 == 1):  # ¿Para qué sirve este loop? ¿Es para termalizar?
         for i in range(100):
             x[0] = ev_posicion(Delta_s, kappa[0], theta[0], x[0])
 
@@ -26,31 +26,35 @@ def proceso(l, Delta_s, kappa, theta, x0):
 
 #Proceso isotermico protocolo 1
 def isoterma_protocolo_1(l, s_0, s_f, theta):
-    s = np.linspace(s_0, s_f, l)
+    s = np.linspace(s_0, s_f, l) 
+# Esto va a generar deltas de tiempo que no son iguales para cada parte del
+# protocolo. No es buena idea tener diferentes delta s para cada parte del
+# protocolo. Eso introduce errores numéricos que no son iguales en cada parte
+# del los procesos. Es mejor usar un solo delta s para todo el ciclo. 
     
     kappa = (1 - 2*s/st)**2*(kappa_A - kappa_C) + kappa_C
     theta = np.ones(l)*theta
-    y = theta/kappa
+    y = theta/kappa # 'y' no se debe determinar así, pues debe salir del promedio sobre muchas simulaciones.
     
-    return [s, kappa, theta, y]
+    return [s, kappa, theta, y] # no está usando 's' en ninguna parte en lo que sigue.
 
 #Proceso adiabatico protocolo 1
 def adiabata_protocolo_1(l, s_0, s_f, theta_0, theta_f):
     s = np.linspace(s_0, s_f, l)
     
     kappa = (1 - 2*s/st)**2*(kappa_A - kappa_C) + kappa_C
-    theta = (theta_f - theta_0)*(s - s_0)/(s_f - s_0) + theta_0
+    theta = (theta_f - theta_0)*(s - s_0)/(s_f - s_0) + theta_0 # no se va a cumplir la relación theta**2/kappa constante, o mejor theta/kappa constante.
     y = theta/kappa
     
     return [s, kappa, theta, y]
 
 #Ciclo protocolo 1
-def protocolo_1(l, cant_iter, kappa_A, y_A, kappa_B, y_B, kappa_C, y_C, kappa_D, y_D, theta_AB, theta_CD):
+def protocolo_1(l, cant_iter, kappa_A, y_A, kappa_B, y_B, kappa_C, y_C, kappa_D, y_D, theta_AB, theta_CD): # los y_A, etc.. no se usan.
     sA = 0
     sB = st/2*(1 - np.sqrt((kappa_B - kappa_C)/(kappa_A - kappa_C)))
     sC = st/2
     sD = st/2*(1 + np.sqrt((kappa_D - kappa_C)/(kappa_A - kappa_C)))
-
+# No usar Delta_s diferentes para cada parte del ciclo. Usar siempre el mismo Delta_s
     Delta_s1 = (sB - sA)/l
     Delta_s2 = (sC - sB)/l
     Delta_s3 = (sD - sC)/l
@@ -66,31 +70,56 @@ def protocolo_1(l, cant_iter, kappa_A, y_A, kappa_B, y_B, kappa_C, y_C, kappa_D,
     var_4 = adiabata_protocolo_1(l, sD, st, theta_CD, theta_AB)
 
     #[s, kappa, theta, y]
-    variables = np.array([var_1, var_2, var_3, var_4])
+    variables = np.array([var_1, var_2, var_3, var_4]) 
     Deltas_s = [Delta_s1, Delta_s2, Delta_s3, Delta_s4]
     
     y_sto = []
     W_sto = []
 
-    x0 = np.ones(cant_iter)*np.sqrt(variables[0][3, 0])
+    x0 = np.ones(cant_iter)*np.sqrt(variables[0][3, 0]) 
+    # ¿qué es variables[0][3, 0]? Está ofuscando los nombres de sus variables y
+    # eso dificulta la lectura del código que depués resulta en posibles bugs.
+    # Es mejor que 'desempaquete' las variables usando:
+    # [s, kappa, theta, y] = var1
+    # Y luego usa y[0] en vez de variables[0][3, 0]
 
-    for i in range(4):
+    # Todo este código se repite multiples veces en cada protocolo. Escriba una
+    # sola función que luego llama desde cada cada protocolo. La función tomará
+    # como argumentos las funciones que definen kappa y theta como función del tiempo  
+    for i in range(4): 
+    # como está barriendo sobre los elementos de 'variables' es mejor usar:
+    # for var in variables:    
         var = variables[i]
         y_proc = np.zeros(l)
         W_proc_stochastic = []
 
+        # aquí repite una sola rama del ciclo y promedia sobre ella. Pero
+        # físicamente tendría más sentido promediar sobre todo el ciclo. Es
+        # decir dejar que la maquina haga un ciclo completo y luego repetir el
+        # ciclo muchas veces.
         for j in range(cant_iter):
+            # estas dos lineas es mejor desempaquetarlas así:
+            # [s, kappa_proc, theta_proc, y] = var
+            # deberían ir afuera del loop sobre j: ¿para que repetir esa
+            # asignación en cada iteración?
             kappa_proc = var[1]
             theta_proc = var[2]
-
+            # hay que usar el mismo Delta_s para todos los procesos.
             x = proceso(l, Deltas_s[i], kappa_proc, theta_proc, x0[j])    
 
             U = (kappa_proc/2)*(x)**2
             W = 0
-            x0[j] = x[-1]
+            x0[j] = x[-1] # No entiendo esto. x0[j] no se vuelve a usar después. ¿Para qué lo asigna a la posición final?
                 
             for k in range(1, l):
-                W += (U[k] - U[k - 1])
+                W += (U[k] - U[k - 1]) # El trabajo está mal calculado. Esto es el cambio total de energía.
+                # Calcular también el calor recibido y expulsado.
+                # Es mejor que el cálculo del trabajo y calor lo haga en la
+                # función 'proceso', pues necesita evaluar la energia para x[i]
+                # y x[i+1] con los cambios correspondientes de k[i] y k[i+1]
+                # En cada paso de simulación el trabajo w y calor q se calculan así:
+                # w = w + U(xnew, t + dt) - U(xnew, t)
+                # q = q + U(xnew, t) - U(xold, t)
                 
             W_proc_stochastic.append(W)
             y_proc += (x)**2
@@ -107,7 +136,7 @@ def protocolo_1(l, cant_iter, kappa_A, y_A, kappa_B, y_B, kappa_C, y_C, kappa_D,
 #Protocolo 2
 
 #Proceso isotermico protocolo 2
-def isoterma_protocolo_2(l, Delta_s, kappa_0, y_0, kappa_f, y_f, theta):
+def isoterma_protocolo_2(l, Delta_s, kappa_0, y_0, kappa_f, y_f, theta): # los 'y' no se usan.
     s_f = l*Delta_s
     s = np.linspace(0, s_f, l)
     
@@ -118,7 +147,7 @@ def isoterma_protocolo_2(l, Delta_s, kappa_0, y_0, kappa_f, y_f, theta):
     return [s, kappa, theta, y]
 
 #Proceso adiabatico protocolo 2
-def adiabata_protocolo_2(l, Delta_s, kappa_0, y_0, theta_0, kappa_f, y_f, theta_f):
+def adiabata_protocolo_2(l, Delta_s, kappa_0, y_0, theta_0, kappa_f, y_f, theta_f): # los 'y' no se usan.
     s_f = l*Delta_s
     s = np.linspace(0, s_f, l)
     
@@ -130,6 +159,7 @@ def adiabata_protocolo_2(l, Delta_s, kappa_0, y_0, theta_0, kappa_f, y_f, theta_
     return [s, kappa, theta, y]
 
 #Ciclo protocolo 2
+# repetición de código que ya usó en la anterior.
 def protocolo_2(l, cant_iter, kappa_A, y_A, kappa_B, y_B, kappa_C, y_C, kappa_D, y_D, theta_AB, theta_CD):
     Delta_s1 = st/(4*l)
     Delta_s2 = st/(4*l)
